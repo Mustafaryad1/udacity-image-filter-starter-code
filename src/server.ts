09bb59efea162da983712c5410b694b37566a5d0
Mustafa Ryad <mustafa.ryad@emushrif.om>
2023-01-06 23:57:12 +0200

    Implement endpoint to filter image

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express from "express";
+import bodyParser from "body-parser";
+import {
+  filterImageFromURL,
+  deleteLocalFiles,
+  checkImageURL,
+} from "./util/util";
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,17 +33,28 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/", async (req, res) => {
+    const imageURL: string = req.query.image_url;
+
+    if (!checkImageURL(imageURL))
+      return res
+        .status(422)
+        .send("Invalid image url--->try GET url/?image_url={{}}");
+
+    const path = await filterImageFromURL(imageURL);
+
+    res.sendFile(path);
+    res.on("finish", () => {
+      deleteLocalFiles([path])
+    });
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
